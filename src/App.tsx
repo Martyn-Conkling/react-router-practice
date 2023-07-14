87ecb83dfc7aa7544ed1eab3b6c79cd01f3c7763
Martyn-Conkling <23250543+Martyn-Conkling@users.noreply.github.com>
2023-07-13 19:17:27 -0700

    creating some basic pages for the van app

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
+
+import Home from "./pages/Home"
+import About from "./pages/About"
+import Vans from "./pages/Vans"
+
+import './App.css'
+import "./server"
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <BrowserRouter>
+      <header>
+        <Link className="site-logo" to="/">#VanLife</Link>
+        <nav>
+          <Link to="/about">About</Link>
+          <Link to="/vans">Vans</Link>
+        </nav>
+      </header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/vans" element={<Vans />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
